ada6a1ac5472e12d3c02f8345b6fb77f0fcf6a49
chengruixia <chengruixiait@163.com>
2018-01-09 17:23:06 +0800

    new a test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+import sys
+if __name__ == "__main__":
+#     print("这是第一个pycharm下的python3示例代码")
+
+     print("命令行参数个数：%d" % len(sys.argv))
+
+     print("命令行参数列表：%s" % str(sys.argv))
+
+
+    #键盘输入字符串数据
+    # data = input("请输入一串任意字符：")
+     #print("你输入了：%s"% data)
+
+    #以空格分割键盘输入的字符串
+     #listdata=data.split(' ')
+     #print(listdata)
+
